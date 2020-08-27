8b83db987d7aeba34559f63676859886fb43333e
Angel Gabriel Ascanio Duran <developer.gabo@gmail.com>
2020-08-27 17:04:28 -0500

    Initial Commit

---
+++ BookNexosSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookNexosSwiftUI
+//
+//  Created by Angel Gabriel Ascanio Duran on 27/08/20.
+//  Copyright © 2020 Angel Gabriel Ascanio Duran. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
